8d9ae7d4cab9da02e5b8178518cbb291fb397526
levin li <3259963@qq.com>
2022-10-06 09:27:20 +0800

    use `requests` implement

---
+++ src/pyinpark/cmcloud2.py
@@ -0,0 +1,37 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def login():
+    return requests.get(os.getenv("LOGIN_URL"))
+
+
+def auth(login_res):
+    return requests.post(
+        os.getenv("AUTH_URL"),
+        headers={"X-CSRFToken": login_res.cookies["csrftoken"],},
+        cookies=login_res.cookies,
+        # cspell: disable-next-line
+        data={"username": os.getenv("USR"), "password": os.getenv("PWD")},
+    )
+
+
+def query(auth_res, sql):
+    return requests.post(
+        os.getenv("QUERY_URL"),
+        headers={"X-CSRFToken": auth_res.cookies["csrftoken"],},
+        cookies=auth_res.cookies,
+        data={
+            "db_name": os.getenv("DB_NAME"),
+            "instance_name": os.getenv("INSTANCE_NAME"),
+            "limit_num": 0,
+            "schema_name": "",
+            # cspell: disable-next-line
+            "sql_content": sql,
+            "tb_name": "",
+        },
+    )
+
