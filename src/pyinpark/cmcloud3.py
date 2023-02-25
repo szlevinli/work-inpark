d64962cbced668e6d74ee560570910089d0cb063
levin li <3259963@qq.com>
2023-02-25 12:30:10 +0800

    add executor for lease rent

--- src/pyinpark/cmcloud3.py
+++ src/pyinpark/cmcloud3.py
@@ -1,5 +1,6 @@
 import requests
 import toolz.curried as tz
+import os
 
 
 def login(login_url):
@@ -34,3 +35,21 @@ def query(query_url, db_name, instance_name, auth_res, sql):
         },
     )
 
+
+def create_sql_executor_for_leaseRent(load_env_function):
+    load_env_function()
+
+    login_url = os.getenv("LOGIN_URL")
+    auth_url = os.getenv("AUTH_URL")
+    usr = os.getenv("USR")
+    pwd = os.getenv("PWD2")
+    query_url = os.getenv("QUERY_URL")
+    db_name = os.getenv("DB_NAME")
+    instance_name = os.getenv("INSTANCE_NAME")
+
+    return tz.pipe(
+        login_url,
+        login,
+        auth(auth_url, usr, pwd),
+        query(query_url, db_name, instance_name),
+    )
